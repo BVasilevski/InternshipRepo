4ceae5d3ce0c07fd2fce1128374e8947e419022d
Bojan Vasilevski <thegamer3212@gmail.com>
2025-04-14 09:54:59 +0200

    Finished url shortener

--- url-shortener/src/main/kotlin/com/sorsix/urlshortener/controller/UrlShortenerController.kt
+++ url-shortener/src/main/kotlin/com/sorsix/urlshortener/controller/UrlShortenerController.kt
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -12,10 +13,14 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api")
 class UrlShortenerController(val urlService: UrlService) {
-    @GetMapping("/shorturl")
+    @PostMapping("/shorturl")
     fun shortenUrl(@RequestParam url: String): ResponseEntity<Any> {
-        val shortenedUrl = urlService.shortenUrl(url)
-        return ResponseEntity.ok(mapOf("original_url" to url, "short_url" to shortenedUrl?.id));
+        try {
+            val shortenedUrl = urlService.shortenUrl(url)
+            return ResponseEntity.ok(mapOf("original_url" to url, "short_url" to shortenedUrl?.id));
+        } catch (e: RuntimeException) {
+            return ResponseEntity.badRequest().body(mapOf("error" to e.message))
+        }
     }
 
     @GetMapping("/shorturl/{shortUrl}")
--- url-shortener/src/main/kotlin/com/sorsix/urlshortener/service/UrlService.kt
+++ url-shortener/src/main/kotlin/com/sorsix/urlshortener/service/UrlService.kt
@@ -10,6 +10,15 @@ class UrlService(val urlRepository: UrlRepository) {
     fun findById(id: Long): Url = urlRepository.findById(id).orElseThrow { RuntimeException("Short url $id not found") }
 
     fun shortenUrl(originalUrl: String): Url? {
+        if (!isValidHttpUrl(originalUrl)) {
+            throw RuntimeException("invalid url")
+        }
         return urlRepository.save(Url(0, originalUrl))
+    }
+
+    fun isValidHttpUrl(url: String): Boolean {
+        val regex =
+            Regex("""https?:\/\/(?:www\.)?[a-zA-Z0-9\-._~%]+(?:\.[a-zA-Z]{2,})(?::\d+)?(?:\/[^\s?#]*)?(?:\?[^\s#]*)?(?:#[^\s]*)?""")
+        return regex.matches(url)
     }
 }
